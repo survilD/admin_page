b15069073618520f3cfbc428b7a4d2d5b2f663c9
Survil <survil.easternts@gmail.com>
2022-06-08 08:51:10 +0530

    second

--- lib/constants.dart
+++ lib/constants.dart
@@ -6,3 +6,4 @@ const kSecondaryColor = Color.fromRGBO(32, 25, 48, 1);
 const kLightColorGrey = Color.fromRGBO(125, 133, 148, 1);
 const kGreen = Color.fromRGBO(14, 73, 68, 1);
 const kGrey = Color.fromRGBO(125, 133, 148, 1);
+const kclean = Colors.white;
