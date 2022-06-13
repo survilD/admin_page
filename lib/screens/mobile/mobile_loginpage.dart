01aa7f9c9cda3aba6cdc2d30d1e0ef5d7f117c15
survil Dhaduk <survil.easternts@gmail.com>
2022-06-13 14:27:50 +0530

    final

--- lib/screens/mobile/mobile_loginpage.dart
+++ lib/screens/mobile/mobile_loginpage.dart
@@ -1,97 +1,98 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_training_1/screens/mobile/mobile_home.dart';
-import 'package:flutter_training_1/screens/utils/widgets.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_training_1/screens/mobile/mobile_home.dart';
+// import 'package:flutter_training_1/screens/utils/widgets.dart';
 
-class MobileLogin extends StatefulWidget {
-  MobileLogin({Key? key, required this.constraints}) : super(key: key);
-  final BoxConstraints constraints;
+// class MobileLogin extends StatefulWidget {
+//   MobileLogin({Key? key, required this.constraints}) : super(key: key);
+//   final BoxConstraints constraints;
 
-  @override
-  State<MobileLogin> createState() => _MobileLoginState();
-}
+//   @override
+//   State<MobileLogin> createState() => _MobileLoginState();
+// }
 
-class _MobileLoginState extends State<MobileLogin> {
-  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+// class _MobileLoginState extends State<MobileLogin> {
+//   final GlobalKey<FormState> _key = GlobalKey<FormState>();
 
-  final TextEditingController _controllerUsername = TextEditingController();
+//   final TextEditingController _controllerUsername = TextEditingController();
 
-  final TextEditingController _controllerPassword = TextEditingController();
+//   final TextEditingController _controllerPassword = TextEditingController();
 
-  validate(BoxConstraints constraints) {
-    if (_key.currentState!.validate()) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => MobileHome(
-                  name: _controllerUsername.text.toString(),
-                )),
-      );
-    }
-  }
+//   validate(BoxConstraints constraints) {
+//     if (_key.currentState!.validate()) {
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(
+//             builder: (context) => MobileHome(
+               
+//                   name: _controllerUsername.text.toString(),
+//                 )),
+//       );
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    final BoxConstraints constraints = widget.constraints;
-    return SafeArea(
-      child: Scaffold(
-          body: SingleChildScrollView(
-        child: Form(
-          key: _key,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                Text(
-                  "Welcome Back",
-                  style: TextStyle(
-                      color: Colors.grey[800],
-                      fontSize: 35,
-                      fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Icon(
-                  Icons.person,
-                  color: Colors.red,
-                  size: 120,
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                CustomWidgets.loginForm(
-                    controller: _controllerUsername,
-                    titel: "Enter UserName",
-                    error: "Please Enter UserName"),
-                const SizedBox(
-                  height: 20,
-                ),
-                CustomWidgets.loginForm(
-                    controller: _controllerPassword,
-                    error: "Password Is Empty",
-                    titel: "Enter Password"),
-                const SizedBox(
-                  height: 50,
-                ),
-                SizedBox(
-                    width: 200,
-                    height: 50,
-                    child: ElevatedButton(
-                      child: const Text(
-                        "Submit",
-                        style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
-                      ),
-                      onPressed: validate(constraints),
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.red,
-                      ),
-                    ))
-              ],
-            ),
-          ),
-        ),
-      )),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     final BoxConstraints constraints = widget.constraints;
+//     return SafeArea(
+//       child: Scaffold(
+//           body: SingleChildScrollView(
+//         child: Form(
+//           key: _key,
+//           child: Padding(
+//             padding: const EdgeInsets.all(10.0),
+//             child: Column(
+//               children: [
+//                 Text(
+//                   "Welcome Back",
+//                   style: TextStyle(
+//                       color: Colors.grey[800],
+//                       fontSize: 35,
+//                       fontWeight: FontWeight.bold),
+//                 ),
+//                 const SizedBox(
+//                   height: 30,
+//                 ),
+//                 const Icon(
+//                   Icons.person,
+//                   color: Colors.red,
+//                   size: 120,
+//                 ),
+//                 const SizedBox(
+//                   height: 40,
+//                 ),
+//                 CustomWidgets.loginForm(
+//                     controller: _controllerUsername,
+//                     titel: "Enter UserName",
+//                     error: "Please Enter UserName"),
+//                 const SizedBox(
+//                   height: 20,
+//                 ),
+//                 CustomWidgets.loginForm(
+//                     controller: _controllerPassword,
+//                     error: "Password Is Empty",
+//                     titel: "Enter Password"),
+//                 const SizedBox(
+//                   height: 50,
+//                 ),
+//                 SizedBox(
+//                     width: 200,
+//                     height: 50,
+//                     child: ElevatedButton(
+//                       child: const Text(
+//                         "Submit",
+//                         style: TextStyle(
+//                             fontSize: 20, fontWeight: FontWeight.bold),
+//                       ),
+//                       onPressed: validate(constraints),
+//                       style: ElevatedButton.styleFrom(
+//                         primary: Colors.red,
+//                       ),
+//                     ))
+//               ],
+//             ),
+//           ),
+//         ),
+//       )),
+//     );
+//   }
+// }
--- lib/screens/tablet/tablet_loginpage.dart
+++ lib/screens/tablet/tablet_loginpage.dart
@@ -1,99 +1,99 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_training_1/screens/mobile/mobile_home.dart';
-import 'package:flutter_training_1/screens/tablet/tablet_home.dart';
-import 'package:flutter_training_1/screens/utils/widgets.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_training_1/screens/mobile/mobile_home.dart';
+// import 'package:flutter_training_1/screens/tablet/tablet_home.dart';
+// import 'package:flutter_training_1/screens/utils/widgets.dart';
 
-class TabletLoginPage extends StatefulWidget {
-  const TabletLoginPage({Key? key}) : super(key: key);
+// class TabletLoginPage extends StatefulWidget {
+//   const TabletLoginPage({Key? key}) : super(key: key);
 
-  @override
-  State<TabletLoginPage> createState() => _MobileLoginState();
-}
+//   @override
+//   State<TabletLoginPage> createState() => _MobileLoginState();
+// }
 
-class _MobileLoginState extends State<TabletLoginPage> {
-  final GlobalKey<FormState> _key = GlobalKey<FormState>();
+// class _MobileLoginState extends State<TabletLoginPage> {
+//   final GlobalKey<FormState> _key = GlobalKey<FormState>();
 
-  final TextEditingController _controllerUsername = TextEditingController();
+//   final TextEditingController _controllerUsername = TextEditingController();
 
-  final TextEditingController _controllerPassword = TextEditingController();
+//   final TextEditingController _controllerPassword = TextEditingController();
 
-  void validate() {
-    if (_key.currentState!.validate()) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => TablateHome(
-                  name: _controllerUsername.text.toString(),
-                )),
-      );
-    }
-  }
+//   void validate() {
+//     if (_key.currentState!.validate()) {
+//       Navigator.push(
+//         context,
+//         MaterialPageRoute(
+//             builder: (context) => TablateHome(
+//                   name: _controllerUsername.text.toString(),
+//                 )),
+//       );
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: LayoutBuilder(
-          builder: ((context, constraints) => SingleChildScrollView(
-                child: Form(
-                  key: _key,
-                  child: Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          "Welcome Back",
-                          style: TextStyle(
-                              color: Colors.grey[800],
-                              fontSize: 35,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        const Icon(
-                          Icons.person,
-                          color: Colors.red,
-                          size: 120,
-                        ),
-                        const SizedBox(
-                          height: 40,
-                        ),
-                        CustomWidgets.loginForm(
-                            controller: _controllerUsername,
-                            titel: "Enter UserName",
-                            error: "Please Enter UserName"),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        CustomWidgets.loginForm(
-                            controller: _controllerPassword,
-                            error: "Password Is Empty",
-                            titel: "Enter Password"),
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        SizedBox(
-                            width: 200,
-                            height: 50,
-                            child: ElevatedButton(
-                              child: const Text(
-                                "Submit",
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
-                              ),
-                              onPressed: validate,
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.red,
-                              ),
-                            ))
-                      ],
-                    ),
-                  ),
-                ),
-              )),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//         body: LayoutBuilder(
+//           builder: ((context, constraints) => SingleChildScrollView(
+//                 child: Form(
+//                   key: _key,
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(10.0),
+//                     child: Column(
+//                       children: [
+//                         Text(
+//                           "Welcome Back",
+//                           style: TextStyle(
+//                               color: Colors.grey[800],
+//                               fontSize: 35,
+//                               fontWeight: FontWeight.bold),
+//                         ),
+//                         const SizedBox(
+//                           height: 30,
+//                         ),
+//                         const Icon(
+//                           Icons.person,
+//                           color: Colors.red,
+//                           size: 120,
+//                         ),
+//                         const SizedBox(
+//                           height: 40,
+//                         ),
+//                         CustomWidgets.loginForm(
+//                             controller: _controllerUsername,
+//                             titel: "Enter UserName",
+//                             error: "Please Enter UserName"),
+//                         const SizedBox(
+//                           height: 20,
+//                         ),
+//                         CustomWidgets.loginForm(
+//                             controller: _controllerPassword,
+//                             error: "Password Is Empty",
+//                             titel: "Enter Password"),
+//                         const SizedBox(
+//                           height: 50,
+//                         ),
+//                         SizedBox(
+//                             width: 200,
+//                             height: 50,
+//                             child: ElevatedButton(
+//                               child: const Text(
+//                                 "Submit",
+//                                 style: TextStyle(
+//                                     fontSize: 20, fontWeight: FontWeight.bold),
+//                               ),
+//                               onPressed: validate,
+//                               style: ElevatedButton.styleFrom(
+//                                 primary: Colors.red,
+//                               ),
+//                             ))
+//                       ],
+//                     ),
+//                   ),
+//                 ),
+//               )),
+//         ),
+//       ),
+//     );
+//   }
+// }
